4b68e47c7d737f95b3a5a8f31e35fdcd5fe66a5b
Lionel Alliaj <la92@afsconnect2.njit.edu>
2021-03-15 21:39:08 -0400

    new

--- User/login.php
+++ User/login.php
@@ -8,21 +8,21 @@ $db = $database->getConnection();
 
 $user = new User($db);
 
-$user->email = isset($_POST["email"]) ? $_POST["email"] : die();
-$user->user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : die();
-$user->full_name = isset($_POST["full_name"]) ? $_POST["full_name"] : die();
+$user->email = isset($_POST["email"]) ? $_POST["email"] : echo "ERROR");
+$user->user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : echo "ERROR");
+$user->full_name = isset($_POST["full_name"]) ? $_POST["full_name"] : "ERROR");
 //$user->username = isset($_GET['username']) ? $_GET['username'] : die();
-$user->password = base64_encode(isset($_POST["password"]) ? $_POST["password"] : die());
+$user->password = base64_encode(isset($_POST["password"]) ? $_POST["password"] : echo "ERROR");
 
 $stmt = $user->login();
 if($stmt->rowCount() > 0){
 	$row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 	$user_arr = array(
-		"status"=>true,
-		"message" => "Successfully login!", 
-		"user_id" => $row['user_id'],
-		"full_name" => $row['full_name'],
+		//"status"=>true,
+		//"message" => "Successfully login!", 
+		//"user_id" => $row['user_id'],
+		//"full_name" => $row['full_name'],
 		"email" => $row['email'],
 		"password" => $row['password']
 
