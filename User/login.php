0a0ad8b6a06892e3d6796e9d66c4ddc489206350
Lionel Alliaj <la92@afsconnect2.njit.edu>
2021-03-15 21:12:21 -0400

    changes in login.php

--- User/login.php
+++ User/login.php
@@ -8,11 +8,11 @@ $db = $database->getConnection();
 
 $user = new User($db);
 
-$user->email = isset($_POST['email']) ? $_POST['email'] : die();
-
-//$user->full_name = isset($_POST['full_name']) ? $_POST['full_name'] : die();
+$user->email = isset($_POST["email"]) ? $_POST["email"] : die();
+$user->user_id = isset($_POST["user_id"]) ? $_POST["user_id"] : die();
+$user->full_name = isset($_POST["full_name"]) ? $_POST["full_name"] : die();
 //$user->username = isset($_GET['username']) ? $_GET['username'] : die();
-$user->password = base64_encode(isset($_POST['password']) ? $_POST['password'] : die());
+$user->password = base64_encode(isset($_POST["password"]) ? $_POST["password"] : die());
 
 $stmt = $user->login();
 if($stmt->rowCount() > 0){
@@ -22,7 +22,7 @@ if($stmt->rowCount() > 0){
 		"status"=>true,
 		"message" => "Successfully login!", 
 		"user_id" => $row['user_id'],
-		//"full_name" => $row['full_name'],
+		"full_name" => $row['full_name'],
 		"email" => $row['email'],
 		"password" => $row['password']
 
