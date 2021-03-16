1abe36d69c0eaffc4816fb1a9c0859b6a2e39808
Lionel Alliaj <la92@afsconnect2.njit.edu>
2021-03-15 21:07:12 -0400

    new commits

--- User/login.php
+++ User/login.php
@@ -10,7 +10,7 @@ $user = new User($db);
 
 $user->email = isset($_POST['email']) ? $_POST['email'] : die();
 
-$user->full_name = isset($_POST['full_name']) ? $_POST['full_name'] : die();
+//$user->full_name = isset($_POST['full_name']) ? $_POST['full_name'] : die();
 //$user->username = isset($_GET['username']) ? $_GET['username'] : die();
 $user->password = base64_encode(isset($_POST['password']) ? $_POST['password'] : die());
 
@@ -22,7 +22,7 @@ if($stmt->rowCount() > 0){
 		"status"=>true,
 		"message" => "Successfully login!", 
 		"user_id" => $row['user_id'],
-		"full_name" => $row['full_name'],
+		//"full_name" => $row['full_name'],
 		"email" => $row['email'],
 		"password" => $row['password']
 
